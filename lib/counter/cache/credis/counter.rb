d8c637c004d30c338381e8e0e5a187e2bd945b86
linjunzhu <linjunzhugg@gmail.com>
2015-05-20 17:20:56 +0800

    支持同一model多counter

--- lib/counter/cache/credis/counter.rb
+++ lib/counter/cache/credis/counter.rb
@@ -8,19 +8,21 @@ module Counter
 
       module ClassMethods
 
-        def defind_column_getter
+        def defind_column_getter(column)
           self.class_eval do
-            define_method("get_#{self.column}_cache") do
-              self.send(self.column) + Redis.new.get("#{self.class.table_name}/#{self.id}").to_i
+            define_method("get_#{column}_cache") do
+              self.send(column) + Redis.new.get("#{self.class.table_name}/#{column}#{self.id}").to_i
             end
           end
         end
 
         def counter_cache_redis(options = {})
-          mattr_accessor :counter_delay, :column
-          self.counter_delay = options[:delay] || 20
-          self.column = options[:column] || 'views_count'
-          defind_column_getter if options[:column]
+          mattr_accessor :column_delay
+          delay = options[:delay] || 20
+          column = options[:column] || 'views_count'
+          self.column_delay = {} if self.column_delay.nil?
+          self.column_delay[column] = delay
+          defind_column_getter(column)
           include Counter::Cache::Credis::InstanceMethods
         end
 
@@ -29,22 +31,20 @@ module Counter
       module InstanceMethods
 
         # 修改值
-        def update_counter
+        def update_counter(column = 'views_count')
           redis = RedisCli.new
-          # 先读出redis中的值
-          views_count_redis = redis.get("#{self.class.table_name}/#{self.id}").to_i
+          views_count_redis = redis.get("#{self.class.table_name}/#{column}#{self.id}").to_i
           views_count_redis = 0 if !views_count_redis
           views_count_redis += 1
-
-          if views_count_redis >= self.counter_delay
+          if views_count_redis >= column_delay[column]
             # 计算出总读数
-            views_count_temp = views_count_redis + (self.send(self.column) || 0)
+            views_count_temp = views_count_redis + (self.send(column) || 0)
             views_count_redis = 0
-            self.send("#{self.column}=", views_count_temp)
+            self.send("#{column}=", views_count_temp)
             self.save
           end
 
-          redis.set("#{self.class.table_name}/#{self.id}", views_count_redis)
+          redis.set("#{self.class.table_name}/#{column}#{self.id}", views_count_redis)
 
         end
       end
--- lib/counter/cache/credis/version.rb
+++ lib/counter/cache/credis/version.rb
@@ -1,7 +1,7 @@
 module Counter
   module Cache
     module Credis
-      VERSION = "0.0.2"
+      VERSION = "0.0.3"
     end
   end
 end
